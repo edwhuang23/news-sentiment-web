e413675eea9afebbdb3a75cd220d0f374bbfd5ce
edwhuang <edwhuang@umich.edu>
2020-07-20 23:36:13 -0400

    Styling and formatting improvements; added routing with parameters

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if request.method == 'GET':
-        return render_template("home.html")
-    return redirect(url_for('analyze'))
+    if request.method == 'POST':
+        textInput = request.form.get("articleText")
+        return redirect(url_for('analyze', text=textInput))
+
+    return render_template("home.html")
+
 
 @app.route("/analyze")
 def analyze():
-    return "Analysis..."
+    return "Analysis of: " + request.args['text']
 
 if __name__ == "__main__":
     app.run(debug=True)
