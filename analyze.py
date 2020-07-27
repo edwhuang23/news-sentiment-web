8f11bbbc0220228e4e5ddb8a2a471fa0b830225d
edwhuang <edwhuang@umich.edu>
2020-07-27 00:29:50 -0400

    persist trained text model

--- analyze.py
+++ analyze.py
@@ -1,4 +1,9 @@
+import pickle
 
+def restoreTextModel():
+    # Restore tuple
+    pickled_model, pickled_Xtrain, pickled_Ytrain, pickled_score = pickle.load(open("text_model.pkl", 'rb'))
+    return pickled_model, pickled_Xtrain, pickled_Ytrain, pickled_score
 
 def textAnalyze(text):
     return 1
